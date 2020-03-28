633b456a46d27ffc0519015d2d2445bee3b0993d
Adams408 <jnadams408@gmail.com>
2020-03-28 19:57:38 -0400

    new

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import socket
+import ssl
 import tkinter as tk
 import json
+import base64
 
 
 class GUI(tk.Tk):
 
+    # set up the environment and frames to be made and displayed
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
@@ -16,7 +19,7 @@ class GUI(tk.Tk):
 
         self.frames = {}
 
-        for F in (Login, Doctor, Nurse, Patient, Add, Info):
+        for F in (Login, Doctor, Nurse, Patient, Info, Add):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
@@ -24,6 +27,7 @@ class GUI(tk.Tk):
         self.user = None
         self.show_frame(Login)
 
+    # raise and shows the given frame
     def show_frame(self, f):
         self.title(str(f.__name__))
         if f == Doctor or f == Nurse or f == Patient:
@@ -32,18 +36,21 @@ class GUI(tk.Tk):
         frame.tkraise()
         frame.event_generate("<<ShowFrame>>")
 
-    def get_user(self):
-        return self.user.__name__
-
+    # shows the previous frame
     def prev_frame(self):
         self.title(str(self.user.__name__))
         frame = self.frames[self.user]
         frame.tkraise()
         frame.event_generate("<<ShowFrame>>")
 
+    # get the user that has logged in
+    def get_user(self):
+        return self.user.__name__
+
 
 class Login(tk.Frame):
 
+    # initialize the login user interface frame and what it shows
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -60,49 +67,60 @@ class Login(tk.Frame):
 
         tk.Button(self, text='Enter', command=lambda: self.login(controller, username.get() + ' ' + password.get())).grid(row=3, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 5))
 
+    # send the entered user name and password to the server to be authorized
     def login(self, controller, log):
         text = tk.StringVar()
         tk.Label(self, textvariable=text, fg='red').grid(row=4, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S, padx=10, pady=(0, 10))
 
-        if log:
-            client.send('login'.encode())
-            client.send(log.encode())
-            access = client.recv(port).decode()
+        client.send('login'.encode())
+        # encrypts the username and password
+        encrypted = base64.b64encode(log.encode())
+        client.send(encrypted)
+        auth = client.recv(port).decode()
 
-            if access == 'patient':
-                controller.show_frame(Patient)
-            elif access == 'nurse':
-                controller.show_frame(Nurse)
-            elif access == 'doctor':
-                controller.show_frame(Doctor)
-            else:
-                text.set(access)
+        if auth == 'doctor':
+            controller.show_frame(Doctor)
+        elif auth == 'nurse':
+            controller.show_frame(Nurse)
+        elif auth == 'patient':
+            controller.show_frame(Patient)
         else:
-            text.set('You are required to \nenter a username and password.')
+            text.set(auth)
 
 
+# display all patient names in a listbox
 def display(lb):
     client.send('all_patient_names'.encode())
     names = json.loads(client.recv(port).decode())
 
     lb.delete(0, tk.END)
     for i in range(len(names)):
-        lb.insert(i, names[i][0])
+        lb.insert(i, names[i][0] + ' ' + names[i][1])
 
 
+# re enables the view/edit button when a name is selected in the listbox
 def activate(btn):
     btn.configure(state=tk.NORMAL)
 
 
-def name_to_id(controller, name):
+# find the current id of the selected name
+def name_to_id(name):
     if name:
         client.send('name_to_id'.encode())
         client.send(str(name).encode())
-        controller.show_frame(Info)
+        # controller.show_frame(Info)
+
+
+# removes the selected patient name from the listbox
+def remove(lb):
+    name_to_id(lb.get(lb.curselection()))
+    lb.delete(lb.curselection())
+    client.send('remove_patient'.encode())
 
 
 class Doctor(tk.Frame):
 
+    # initialize the doctor user interface frame and what it shows
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -116,22 +134,20 @@ class Doctor(tk.Frame):
 
         tk.Label(self, text='Doctor Options').pack(pady=(0, 5))
 
-        btn = tk.Button(self, text='View/Edit', state=tk.DISABLED, command=lambda: name_to_id(controller, lb.get(lb.curselection())))
-        btn.pack(pady=(0, 5))
+        b1 = tk.Button(self, text='View/Edit', state=tk.DISABLED, command=lambda: (name_to_id(lb.get(lb.curselection())), controller.show_frame(Info)))
+        b1.pack(pady=(0, 5))
 
-        lb.bind("<<ListboxSelect>>", lambda event: activate(btn))
+        b2 = tk.Button(self, text='Remove Patient', state=tk.DISABLED, command=lambda: remove(lb))
+        b2.pack(pady=(0, 5))
+
+        lb.bind("<<ListboxSelect>>", lambda event: (activate(b1), activate(b2)))
 
         tk.Button(self, text='Logout', command=lambda: controller.show_frame(Login)).pack(pady=(0, 10))
 
 
-def remove(controller, lb):
-    lb.delete(lb.curselection())
-    name_to_id(controller, lb.get(lb.curselection()))
-    client.send('remove_patient'.encode())
-
-
 class Nurse(tk.Frame):
 
+    # initialize the nurse user interface frame and what it shows
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -145,18 +161,22 @@ class Nurse(tk.Frame):
 
         tk.Label(self, text='Nurse Options').pack(pady=(0, 5))
 
-        b = tk.Button(self, text='View/Edit', state=tk.DISABLED, command=lambda: name_to_id(controller, lb.get(lb.curselection())))
-        b.pack(pady=(0, 5))
-
-        lb.bind("<<ListboxSelect>>", lambda event: activate(b))
+        b1 = tk.Button(self, text='View/Edit', state=tk.DISABLED, command=lambda: (name_to_id(lb.get(lb.curselection())), controller.show_frame(Info)))
+        b1.pack(pady=(0, 5))
 
         tk.Button(self, text='Add Patient', command=lambda: controller.show_frame(Add)).pack(pady=(0, 5))
-        tk.Button(self, text='Remove Patient', command=lambda: remove(controller, lb)).pack(pady=(0, 5))
+
+        b2 = tk.Button(self, text='Remove Patient', state=tk.DISABLED, command=lambda: remove(lb))
+        b2.pack(pady=(0, 5))
+
+        lb.bind("<<ListboxSelect>>", lambda event: (activate(b1), activate(b2)))
+
         tk.Button(self, text='Logout', command=lambda: controller.show_frame(Login)).pack(pady=(0, 10))
 
 
 class Patient(tk.Frame):
 
+    # initialize the patient user interface frame and what it shows
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -166,52 +186,7 @@ class Patient(tk.Frame):
         tk.Button(self, text='Logout', command=lambda: controller.show_frame(Login)).pack(pady=(0, 10))
 
 
-def add(controller, entries, text):
-    ent = []
-    for i in range(len(entries)):
-        ent.append(entries[i].get())
-
-    if '' in ent:
-        text.set('Please set a value all fields. Enter 0 for \nbalance if there is no account balance.')
-    else:
-        client.send('add_patient'.encode())
-        client.send(json.dumps(ent).encode())
-        controller.show_frame(Add)
-
-
-class Add(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        self.sv = tk.StringVar()
-
-        self.bind("<<ShowFrame>>", lambda event: self.display(controller))
-
-    def display(self, controller):
-        tk.Label(self, text='Add New Patient').grid(row=0, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S, padx=10, pady=(10, 5))
-        loc = 1
-
-        # client.send('get_patient_fields'.encode())
-        # info = json.loads(client.recv(port).decode())
-        info = ['Username', 'Password', 'First_Name', 'Last_Name', 'Address', 'Phone Number', 'Balance']
-        entries = []
-
-        for item in info:
-            tk.Label(self, text=item + ':').grid(row=loc, sticky=tk.W, padx=(10, 0), pady=(0, 5))
-            ent = tk.Entry(self)
-            ent.grid(row=loc, column=1, columnspan=2, sticky=tk.W, padx=(0, 10), pady=(0, 5))
-            entries.append(ent)
-            loc += 1
-
-        text = tk.StringVar()
-
-        tk.Button(self, text='Add', command=lambda: add(controller, entries, text)).grid(row=loc, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 5))
-        tk.Button(self, text='Cancel', command=lambda: controller.prev_frame()).grid(row=loc + 1, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 5))
-
-        tk.Label(self, textvariable=text, fg='red').grid(row=loc + 2, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S, padx=10, pady=(0, 10))
-
-
+# save any changes done to the patient info by the doctor or nurse
 def save(entries):
     ent = []
     for i in range(len(entries)):
@@ -221,6 +196,7 @@ def save(entries):
     client.send(json.dumps(ent).encode())
 
 
+# take away or add to balance based on the visit
 def pay(controller, amount, balance):
     try:
         sign = amount[:1]
@@ -236,17 +212,19 @@ def pay(controller, amount, balance):
         client.send(str(total).encode())
         controller.show_frame(Info)
 
-    except Exception as e:
-        print(e)
+    except Exception as ex:
+        print(ex)
 
 
 class Info(tk.Frame):
 
+    # initialize the info user interface frame and what it shows
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
         self.bind("<<ShowFrame>>", lambda event: self.display(controller))
 
+    # when frame is displayed it will get the selected patient info from the server and displays it
     def display(self, controller):
 
         tk.Label(self, text='Information').grid(row=0, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S, padx=10, pady=(10, 5))
@@ -254,17 +232,16 @@ class Info(tk.Frame):
 
         client.send('patient_info'.encode())
         info = json.loads(client.recv(port).decode())
-        fields = ['First Name', 'Last Name', 'Address', 'Phone Number', 'Balance']
+        fields = ['ID', 'First_Name', 'Last_Name', 'Address', 'Phone_Number', 'Balance']
         entries = []
 
         if controller.get_user() != 'Patient':
             for field in fields:
                 if field != 'ID':
+                    tk.Label(self, text=field.replace('_', ' ') + ':').grid(row=loc, sticky=tk.W, padx=(10, 0), pady=(0, 5))
                     if field == 'Balance':
-                        tk.Label(self, text=field + ':').grid(row=loc, sticky=tk.W, padx=(10, 0), pady=(0, 5))
                         tk.Label(self, text=info[0][loc - 1]).grid(row=loc, column=1, columnspan=2, sticky=tk.W, padx=(0, 10), pady=(0, 5))
                     else:
-                        tk.Label(self, text=field + ':').grid(row=loc, sticky=tk.W, padx=(10, 0), pady=(0, 5))
                         ent = tk.Entry(self)
                         ent.insert(0, info[0][loc - 1])
                         ent.grid(row=loc, column=1, columnspan=2, sticky=tk.W, padx=(0, 10), pady=(0, 5))
@@ -277,8 +254,7 @@ class Info(tk.Frame):
             loc += 1
 
             tk.Button(self, text='Save', command=lambda: save(entries)).grid(row=loc, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 5))
-            tk.Button(self, text='Update Balance', command=lambda: pay(controller, amount.get(), info[0][len(info[0]) - 1])).grid(row=loc + 1, column=2, sticky=tk.E, padx=(0, 10),
-                                                                                                                                  pady=(0, 5))
+            tk.Button(self, text='Update Balance', command=lambda: pay(controller, amount.get(), info[0][len(info[0]) - 1])).grid(row=loc + 1, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 5))
             loc += 2
         else:
             tk.Label(self, text='Balance:').grid(row=loc, sticky=tk.W, padx=(10, 0), pady=(0, 5))
@@ -288,20 +264,82 @@ class Info(tk.Frame):
         tk.Button(self, text='Back', command=lambda: controller.prev_frame()).grid(row=loc, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 10))
 
 
+# add new patient based in the inputted data
+def add(controller, entries, text):
+    ent = [entries[0].get(), base64.b64encode(entries[1].get().encode()).decode()]
+    for i in range(2, len(entries)):
+        ent.append(entries[i].get())
+
+    if '' in ent:
+        text.set('Please set a value all fields. Enter 0 for \nbalance if there is no account balance.')
+    else:
+        client.send('add_patient'.encode())
+        client.send(json.dumps(ent).encode())
+        controller.show_frame(Add)
+
+
+class Add(tk.Frame):
+
+    # initialize the add user interface frame and what it shows
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.sv = tk.StringVar()
+
+        self.bind("<<ShowFrame>>", lambda event: self.display(controller))
+
+    # when frame is displayed it will display a blank form for a new patient
+    def display(self, controller):
+        tk.Label(self, text='Add New Patient').grid(row=0, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S, padx=10, pady=(10, 5))
+        loc = 1
+
+        fields = ['Username', 'Password', 'First_Name', 'Last_Name', 'Address', 'Phone Number', 'Balance']
+        entries = []
+
+        for field in fields:
+            tk.Label(self, text=field.replace('_', ' ') + ':').grid(row=loc, sticky=tk.W, padx=(10, 0), pady=(0, 5))
+            ent = tk.Entry(self)
+            ent.grid(row=loc, column=1, columnspan=2, sticky=tk.W, padx=(0, 10), pady=(0, 5))
+            entries.append(ent)
+            loc += 1
+
+        text = tk.StringVar()
+
+        tk.Button(self, text='Add', command=lambda: add(controller, entries, text)).grid(row=loc, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 5))
+        tk.Button(self, text='Cancel', command=lambda: controller.prev_frame()).grid(row=loc + 1, column=2, sticky=tk.E, padx=(0, 10), pady=(0, 5))
+
+        tk.Label(self, textvariable=text, fg='red').grid(row=loc + 2, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S, padx=10, pady=(0, 10))
+
+
 if __name__ == '__main__':
 
     host = 'localhost'
     port = 9999
 
+    # creates the socket to make the client
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connects to the server
     client.connect((host, port))
     print("connected")
 
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.verify_mode = ssl.CERT_REQUIRED
+
+    context.load_verify_locations("./certificate/server.pem")
+    context.load_cert_chain(certfile="./certificate/client.pem", keyfile="./certificate/client.key")
+    
+    if ssl.HAS_SNI:
+        ssl_client = context.wrap_socket(client, server_side=False, server_hostname=host)
+    else:
+        ssl_client = context.wrap_socket(client, server_side=False)
+
     try:
+        # run the gui
         GUI().mainloop()
         client.send('disconnect'.encode())
 
     except Exception as e:
         print(e)
 
+    # ssl_client.close()
     client.close()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 import threading
 import time
 
@@ -12,96 +13,109 @@ class ClientThread(threading.Thread):
         threading.Thread.__init__(self)
 
         self._socket = _socket
-        print("connected to client:", client_addr)
+        print('connected to client:', client_addr)
 
     def run(self):
-        try:
-            while True:
-                data = self._socket.recv(port).decode()
-                if not data:
-                    break
-                print("[%s] %s" % (time.ctime(), data))
 
-                if data == 'login':
-                    log = self._socket.recv(port).decode().split(' ')
-                    u.execute('SELECT ID, Access_Privilege FROM "user" WHERE Username = "%s" and Password = "%s";' % (log[0], log[1]))
-                    q_data = u.fetchone()
-                    if q_data is None:
-                        self._socket.send('Invalid username and password.'.encode())
-                    else:
-                        _id = q_data[0]
-                        self._socket.send(q_data[1].encode())
+        # connect to the database
+        conn_u = sqlite3.connect("./database/user_information.db")
+        conn_p = sqlite3.connect("./database/patient_information.db")
 
-                if data == 'all_patient_names':
-                    p.execute('SELECT First_Name, Last_Name FROM patient;')
-                    self._socket.send(json.dumps(p.fetchall()).encode())
+        u = conn_u.cursor()
+        p = conn_p.cursor()
 
-                if data == 'name_to_id':
-                    name = self._socket.recv(port).decode().split(' ')
-                    p.execute('SELECT ID FROM patient WHERE First_Name = "%s", Last_Name = "%s";' % (name[0], name[1]))
-                    _id = p.fetchone()[0]
+        while True:
+            # receive data from the server
+            data = self._socket.recv(port).decode()
+            if not data:
+                break
+            print('[%s] %s' % (time.ctime(), data))
 
-                if data == 'patient_info':
-                    p.execute('SELECT * FROM patient WHERE ID = "%s";' % _id)
-                    self._socket.send(json.dumps(p.fetchall()).encode())
+            # using the sqlite it will query the table and will authorize if the username and password are correct
+            if data == 'login':
+                auth = self._socket.recv(port).decode()
+                u.execute('SELECT ID, Access_Privilege FROM "user" WHERE "Authorization" = "%s";' % auth)
+                q_data = u.fetchone()
+                if q_data is None:
+                    self._socket.send('Invalid username and password.'.encode())
+                else:
+                    _id = q_data[0]
+                    self._socket.send(q_data[1].encode())
 
-                if data == 'get_patient_fields':
-                    p.execute('PRAGMA table_info(PATIENT)')
-                    self._socket.send(json.dumps(p.fetchalll()).encode())
+            # it will query the table and will get the names of every one in the database
+            if data == 'all_patient_names':
+                p.execute('SELECT First_Name, Last_Name FROM patient;')
+                self._socket.send(json.dumps(p.fetchall()).encode())
 
-                if data == 'add_patient':
-                    info = json.loads(self._socket.recv(port).decode())
-                    u.execute('INSERT INTO "user"(Username, Password, Access_Privilege) VALUES ("%s", "%s", patient);' % (info[0], info[1]))
-                    conn_u.commit()
+            # it will query the table and will get the id of the entered name
+            if data == 'name_to_id':
+                name = self._socket.recv(port).decode().split(' ')
+                p.execute('SELECT ID FROM patient WHERE First_Name = "%s" and Last_Name = "%s";' % (name[0], name[1]))
+                _id = p.fetchone()[0]
 
-                    u.execute('SELECT ID FROM "user" WHERE Username = "%s" and Password = "%s";' % (info[0], info[1]))
-                    _id = u.fetchone()
-                    p.execute('INSERT INTO patient(ID, First_Name, Last_Name, Address, Phone_Number, Balance) VALUES ("%s", "%s", "%s", "%s", "%s", "%s");' % (_id, info[2], info[3], info[4], info[5], info[6]))
-                    conn_p.commit()
+            # it will query the table and will get all the information based of off the id
+            if data == 'patient_info':
+                p.execute('SELECT * FROM patient WHERE ID = "%s";' % _id)
+                self._socket.send(json.dumps(p.fetchall()).encode())
 
-                if data == 'remove_patient':
-                    u.execute('DELETE * FROM "user" WHERE ID = "%s";' % _id)
-                    conn_u.commit()
+            # it will insert the received data into the table
+            if data == 'add_patient':
+                info = json.loads(self._socket.recv(port).decode())
+                u.execute('INSERT INTO "user"(Username, "Authorization", Access_Privilege) VALUES ("%s", "%s", "%s");' % (info[0], info[1], 'patient'))
+                conn_u.commit()
 
-                    p.execute('DELETE * FROM patient WHERE ID = "%s";' % _id)
-                    conn_p.commit()
+                u.execute('SELECT ID FROM "user" WHERE Username = "%s" and Password = "%s";' % (info[0], info[1]))
+                _id = u.fetchone()[0]
+                p.execute('INSERT INTO patient(ID, First_Name, Last_Name, Address, Phone_Number, Balance) VALUES ("%s", "%s", "%s", "%s", "%s", "%s");' % (_id, info[2], info[3], info[4], info[5], info[6]))
+                conn_p.commit()
 
-                if data == 'save':
-                    info = json.loads(self._socket.recv(port).decode())
-                    p.execute('UPDATE patient SET First_Name = "%s", Last_Name = "%s", Address = "%s", Phone_Number = "%s" WHERE ID = "%s";' % (info[0], info[1], info[2], info[3], _id))
-                    conn_p.commit()
+            # it will remove the selected patients data from both tables
+            if data == 'remove_patient':
+                u.execute('DELETE FROM "user" WHERE ID = "%s";' % _id)
+                conn_u.commit()
 
-                if data == 'pay':
-                    pay = self._socket.recv(port).decode()
-                    p.execute('UPDATE patient SET Balance = "%s" WHERE ID = "%s";' % (pay, _id))
-                    conn_p.commit()
+                p.execute('DELETE FROM patient WHERE ID = "%s";' % _id)
+                conn_p.commit()
 
-                if data == 'disconnect':
-                    break
+            # it will insert the received data into the table
+            if data == 'save':
+                info = json.loads(self._socket.recv(port).decode())
+                p.execute('UPDATE patient SET First_Name = "%s", Last_Name = "%s", Address = "%s", Phone_Number = "%s" WHERE ID = "%s";' % (info[0], info[1], info[2], info[3], _id))
+                conn_p.commit()
 
-        except Exception as e:
-            print(e)
+            # it will update the tables balance with the data in the location of the patients id
+            if data == 'pay':
+                pay = self._socket.recv(port).decode()
+                p.execute('UPDATE patient SET Balance = "%s" WHERE ID = "%s";' % (pay, _id))
+                conn_p.commit()
+
+            # disconnect client from server
+            if data == 'disconnect':
+                break
 
         print("client at", addr, "disconnected...")
 
 
 if __name__ == '__main__':
 
-    conn_u = sqlite3.connect('./database/user_information.db')
-    conn_p = sqlite3.connect('./database/patient_information.db')
-
-    u = conn_u.cursor()
-    p = conn_p.cursor()
-
     host = 'localhost'
     port = 9999
 
+    # creates the socket to make the server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # bind the address and the port to the socket
     server.bind((host, port))
 
     while True:
-        server.listen(1)
-        client, addr = server.accept()
-        thread = ClientThread(client, addr)
-        thread.start()
+        try:
+            # listen for any connections
+            server.listen(1)
+            client, addr = server.accept()
+            ssl_server = ssl.wrap_socket(client, server_side=True, ca_certs="./certificate/client.pem", certfile="./certificate/server.pem", keyfile="./certificate/server.key", cert_reqs=ssl.CERT_REQUIRED, ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+            # creates a new thread for each connected client
+            thread = ClientThread(client, addr)
+            thread.start()
+        except Exception as e:
+            print(e)
